b3816a55a05cf82db9109d8fce10ef8fa1a1b09d
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-06-01 15:56:23 +0200

    add solution for all inclusive

--- Perm.exs
+++ Perm.exs
@@ -9,11 +9,12 @@ defmodule Perm do
       |>Enum.map(fn(perm)->[char|perm] end)
     end)
   end
-
-  #do: Enum.flat_map(charlist, fn(char) ->
-  #  Enum.map(permute(List.delete(charlist, char)), fn(perm) -> [char|perm] end)
-  #  end)
   def permutestring(listofperms), do: Enum.map(listofperms, fn(perm)->List.to_string(perm) end)
 
+  def getallperm(string), do: stringtocharlist(string)|>permute|>permutestring
+  def contain_all_rots("", _), do: true
+  def contain_all_rots(strng, arr) do
+    Enum.all?(Enum.map(getallperm(strng), fn(perm)-> Enum.member?(arr, perm) end), fn(x)->x end)
+  end
 
 end
