6c14160dcd271226f6e6bb554fe266f67ed8bf7d
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-06-01 15:01:15 +0200

    make it pretty

--- Perm.exs
+++ Perm.exs
@@ -1,9 +1,18 @@
 defmodule Perm do
   def stringtocharlist(string), do: String.to_char_list(string)
   def permute(''), do: ['']
-  def permute(charlist), do: Enum.flat_map(charlist, fn(char) ->
-    Enum.map(permute(List.delete(charlist, char)), fn(perm) -> [char|perm] end)
+  def permute(charlist) do
+    Enum.flat_map(charlist, fn(char) ->
+      charlist
+      |>List.delete(char)
+      |>permute
+      |>Enum.map(fn(perm)->[char|perm] end)
     end)
+  end
+
+  #do: Enum.flat_map(charlist, fn(char) ->
+  #  Enum.map(permute(List.delete(charlist, char)), fn(perm) -> [char|perm] end)
+  #  end)
   def permutestring(listofperms), do: Enum.map(listofperms, fn(perm)->List.to_string(perm) end)
 
 
