9806bb37a2aba5758da014dc0a6269a36a44088f
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-06-07 16:27:09 +0200

    fixed else case

--- nextbf.exs
+++ nextbf.exs
@@ -2,8 +2,10 @@ defmodule Nextbf do
   def next_item(list, item), do: next_item(list, item, nil)
   def next_item([], _, acc), do: acc
   def next_item([h|tail], item, acc) do
-    if ((h==item) and Enum.count(tail)>0 and (acc == nil)) do next_item(tail,item,List.first(tail))
-  else acc
+    if ((h==item) and Enum.count(tail)>0) do
+      if (acc == nil) do next_item(tail,item,List.first(tail))
+    else acc end
+  else next_item(tail,item,acc)
   end
 end
 
