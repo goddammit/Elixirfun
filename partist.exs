171cfcc21baba3db7e94ae95a619aad1ec010873
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-06-20 13:16:03 +0200

    added sense to the logic

--- partist.exs
+++ partist.exs
@@ -1,10 +1,8 @@
 defmodule Partlist do
-  def stringoflist(alist) do
-    Enum.reduce(alist, fn(elem,next) ->"#{elem} #{next}" end)
-  end
+
   def part_list(a)do
         Enum.map(Range.new(1,Enum.count(a)-1), fn(inx)-> {a,b}=Enum.split(a,inx)
-        [stringoflist(Enum.reverse(a))|[stringoflist(Enum.reverse(b))]] end)
+        [Enum.join(a, " ")|[Enum.join(b," ")]] end)
 
   end
 
