5a0b71d9d6a24aef0479d0dce32e4f0cfe44380b
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-11-28 17:55:49 +0100

    more gcd fixes, still does not pass some weird test cases. no timeouts. hurray?

--- reducebysteps.exs
+++ reducebysteps.exs
@@ -3,9 +3,9 @@ defmodule Reducebystep do
   def som(a,b), do: a+b
   def mini(a,b), do: if a<b, do: a, else: b
   def maxi(a,b), do: if a>b, do: a, else: b
-  def gcdi(a,a), do: abs(a)
-  def gcdi(a,b), do: if abs(a)>abs(b), do: gcdi(abs(a)-abs(b),abs(b)), else: gcdi(abs(a),abs(b)-abs(a))
-  def lcmu(a,b), do: round(a*b/gcdi(abs(a),abs(b)))
+  def gcdi(a,0), do: a
+  def gcdi(a,b), do: gcdi(abs(b), rem(a,b))
+  def lcmu(a,b), do: round(abs(a)*abs(b)/gcdi(a,b))
   def oper_array(fun,[head|arr],init), do: oper_array(fun,[head|arr],init, [])
   def oper_array(_,[],_, acc), do: Enum.reverse(acc)
   def oper_array(fun,[head|arr],init,acc), do: oper_array(fun,arr,fun.(head,init), [fun.(head,init)|acc])
