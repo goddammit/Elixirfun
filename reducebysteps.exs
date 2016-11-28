79598032d9ad660e06202d246a6d05b8fde75f33
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-11-28 17:29:16 +0100

    fixed gcd for n<0(bloody hell)

--- reducebysteps.exs
+++ reducebysteps.exs
@@ -3,11 +3,10 @@ defmodule Reducebystep do
   def som(a,b), do: a+b
   def mini(a,b), do: if a<b, do: a, else: b
   def maxi(a,b), do: if a>b, do: a, else: b
-  def gcdi(a,a), do: a
-  def gcdi(a,b), do: if a>b, do: gcdi(a-b,b), else: gcdi(a,b-a)
-  def lcmu(a,b), do: round(a*b/gcdi(a,b))
+  def gcdi(a,a), do: abs(a)
+  def gcdi(a,b), do: if abs(a)>abs(b), do: gcdi(abs(a)-abs(b),abs(b)), else: gcdi(abs(a),abs(b)-abs(a))
+  def lcmu(a,b), do: round(a*b/gcdi(abs(a),abs(b)))
   def oper_array(fun,[head|arr],init), do: oper_array(fun,[head|arr],init, [])
   def oper_array(_,[],_, acc), do: Enum.reverse(acc)
   def oper_array(fun,[head|arr],init,acc), do: oper_array(fun,arr,fun.(head,init), [fun.(head,init)|acc])
-  #def oper_array(fun,[head|arr],init), do: [fun.(head,init)|oper_array(fun,arr,fun.(head,init))]
 end
