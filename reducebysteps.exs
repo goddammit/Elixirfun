bc0bd2951618ab785f7e2c337054e2323d785cd7
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-11-28 16:16:24 +0100

    added tail recurrency, codewars still timeout:/

--- reducebysteps.exs
+++ reducebysteps.exs
@@ -5,6 +5,8 @@ defmodule Reducebystep do
   def gcdi(a,a), do: a
   def gcdi(a,b), do: if a>b, do: gcdi(a-b,b), else: gcdi(a,b-a)
   def lcmu(a,b), do: round(a*b/gcdi(a,b))
-  def oper_array(fun,[],init), do: []
-  def oper_array(fun,[head|arr],init), do: [fun.(head,init)|oper_array(fun,arr,fun.(head,init))]
+  def oper_array(fun,[head|arr],init), do: oper_array(fun,[head|arr],init, [])
+  def oper_array(_,[],_, acc), do: Enum.reverse(acc)
+  def oper_array(fun,[head|arr],init,acc), do: oper_array(fun,arr,init, [fun.(head,init)|acc])
+  #def oper_array(fun,[head|arr],init), do: [fun.(head,init)|oper_array(fun,arr,fun.(head,init))]
 end
