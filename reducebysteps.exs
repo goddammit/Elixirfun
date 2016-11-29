a3ceca41aefd32a72f5d8f4a36dea87e084c665b
Katarzyna Kraus <katarzynakraus@Admins-MacBook-Pro-4.local>
2016-11-29 12:29:09 +0100

    changed / to div, turns out it was rounding wrong for big numbers. bloody /

--- reducebysteps.exs
+++ reducebysteps.exs
@@ -3,9 +3,11 @@ defmodule Reducebystep do
   def som(a,b), do: a+b
   def mini(a,b), do: if a<b, do: a, else: b
   def maxi(a,b), do: if a>b, do: a, else: b
-  def gcdi(a,0), do: a
-  def gcdi(a,b), do: gcdi(abs(b), rem(a,b))
-  def lcmu(a,b), do: round(abs(a)*abs(b)/gcdi(a,b))
+  def gcdi(a,b), do: gcdis(abs(a),abs(b))
+  def gcdis(a,0), do: a
+  def gcdis(0,b), do: b
+  def gcdis(a,b), do: gcdis(b,rem(a,b))
+  def lcmu(a,b), do: div(abs(a*b), gcdi(a,b))
   def oper_array(fun,[head|arr],init), do: oper_array(fun,[head|arr],init, [])
   def oper_array(_,[],_, acc), do: Enum.reverse(acc)
   def oper_array(fun,[head|arr],init,acc), do: oper_array(fun,arr,fun.(head,init), [fun.(head,init)|acc])
